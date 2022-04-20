a8a3e6ab1eea2009305c3f5a838ce066e166743d
RISHAV SHARMA <rishavs949@gmail.com>
2022-04-21 00:03:53 +0530

    Add jsx file

---
+++ src/pages/Age.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Age() {
+  return (
+    <div className="comp">
+      <h1>Age</h1>
+    </div>
+  );
+}
+
+export default Age;
---
+++ src/pages/Location.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+function Location() {
+    return ( <div className = "comp" >
+        <h1 > Location </h1>
+        </div>
+    );
+}
+
+export default Location;
