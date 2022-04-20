0030fd4c02e08927e687110a053562e127e3e40e
rishavs949 <rishavs949@gmail.com>
2022-04-21 00:26:54 +0530

    change

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,35 +3,18 @@ import React from "react";
 function Home() {
     return ( <
         div className = "comp" >
-        <
-        table >
-        <
-        tr >
-        <
-        td >
-        Education:
-        <
-        /td> <
-        /tr> <
-        tr >
-        <
-        td >
-        Work Experience:
-        <
-        /td> <
-        /tr> <
-        tr >
-        <
-        td >
-        Personal Skills:
-        <
-        /td> <
-        /tr> <
-        /table>
-
-
-        <
-        /div>
+        <table >
+            <tr >
+                <td >Education:</td>
+            </tr>
+            <tr >
+                <td > Work Experience:</td>
+            </tr>
+            <tr >
+                <td > Personal Skills:</td>
+            </tr>
+        </table>
+        </div>
     );
 }
 
